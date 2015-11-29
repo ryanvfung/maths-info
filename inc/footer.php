a5da539d0a6dc7f344f1a2db194abdbf57970319
Ryan Fung <rvfung@gmail.com>
2015-11-29 19:22:55 +0000

    Update styles and footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,8 @@
 </main></div>
 <div id='footer-wrapper'><footer>
 	<p>Mathematical Information v<?php echo $version; ?></p>
-	<p><a class='link' href='https://github.com/ryanvfung/html'>Ryan Fung's HTML Template 5.1 Hydrazine</a> (<a href='http://validator.w3.org/check?uri=www.ryanvfung.com/maths'>Valid HTML5 &amp; CSS3</a>)</p>
-	<p>Copyright &copy; 2010&ndash;2015 Ryan Fung. All rights reserved.</p>
+	<p><a class='link' href='https://github.com/ryanvfung/html'>Ryan Fung's HTML Template 5.1 Hydrazine</a> (<a class='link' href='http://validator.w3.org/check?uri=www.ryanvfung.com/maths'>Valid HTML5 &amp; CSS3</a>)</p>
+	<p>Copyright &copy; 2010&ndash;<?php echo date('Y'); ?> Ryan Fung. All rights reserved.</p>
 </footer></div>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
